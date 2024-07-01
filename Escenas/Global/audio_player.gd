1e41769c4f13b601a8045dc5f08dc01e2a8c5c61
Juangiox <gioseffijuan@gmail.com>
2024-07-01 05:41:15 -0300

    Escena secundaria agregada

---
+++ Escenas/Global/audio_player.gd
@@ -0,0 +1,6 @@
+extends AudioStreamPlayer2D
+
+
+func play_music():
+	var audio_player = $"."
+	audio_player.play()
---
+++ Escenas/Players/Player_2/Player_2.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	var audio_viejo = $AudioStreamPlayer2D
+	var audio_nuevo = $"../AudioStreamPlayer2D"
+	var area_stop = $"../AreaStop"
+	audio_viejo.stop()
+	audio_nuevo.play()
+	area_stop.queue_free()
+
